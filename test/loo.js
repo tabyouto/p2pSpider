5592307b3442674f5510a16401d27ee0601b94d1
chihaiyue <chihaiyue@111.com.cn>
2017-02-14 16:38:32 +0800

    fix

--- test/loo.js
+++ test/loo.js
@@ -3,9 +3,9 @@
 var sqlAction = require("./mysql.js"); //mysql 配置文件
 var redis = require("redis");
 var client = redis.createClient();
-var i = 1;
+var i = 0;
 function sql() {
-	client.lrange('frameworks', 0, i, function(err, reply) {
+	client.lrange('p2pData', 0, i, function(err, reply) {
 		console.log(reply); // ['angularjs', 'backbone']
 		if(reply) {
 			sqlAction.insert('INSERT IGNORE INTO list(name,magnet,infoHash,size,catch_date,hot,download_count,file_number,content_file) VALUES ?',[JSON.parse(reply[0])],function (err, vals, fields) {
