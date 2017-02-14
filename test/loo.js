8282044fe5cc57d4748a5c2b3e134ff44760c584
chihaiyue <chihaiyue@111.com.cn>
2017-02-14 16:37:36 +0800

    add loop

--- test/loo.js
+++ test/loo.js
@@ -3,12 +3,12 @@
 var sqlAction = require("./mysql.js"); //mysql 配置文件
 var redis = require("redis");
 var client = redis.createClient();
-var i = 0;
+var i = 1;
 function sql() {
 	client.lrange('frameworks', 0, i, function(err, reply) {
 		console.log(reply); // ['angularjs', 'backbone']
 		if(reply) {
-			sqlAction.insert('INSERT IGNORE INTO list(name,magnet,infoHash,size,catch_date,hot,download_count,file_number,content_file) VALUES ?',[JSON.parse(reply)],function (err, vals, fields) {
+			sqlAction.insert('INSERT IGNORE INTO list(name,magnet,infoHash,size,catch_date,hot,download_count,file_number,content_file) VALUES ?',[JSON.parse(reply[0])],function (err, vals, fields) {
 				i++;
 				sql();
 			});
