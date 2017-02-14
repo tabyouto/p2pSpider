c3b99bdbe2ae6e91634cd690f45cbb386bf2c29e
chihaiyue <chihaiyue@111.com.cn>
2017-02-14 14:42:51 +0800

    modify

--- test/index.js
+++ test/index.js
@@ -68,37 +68,37 @@ p2p.on('metadata', function (metadata) {
         tmpArr.push('');
     }
     result.push(tmpArr);
-    console.log(result);
+    //console.log(result);
 
 
 
 
-    //pub.publish('getBt',JSON.stringify(result));
-    //sub.on('message',function(channel,data) {
-    //   console.log(channel,data);
+    pub.publish('getBt',JSON.stringify(result));
+    sub.on('message',function(channel,data) {
+       console.log(channel,data);
+    });
+    sub.subscribe('getBt');
+
+
+    //var msg_count = 0;
+    //
+    //sub.on("subscribe", function (channel, count) {
+    //    pub.publish("a nice channel", "I am sending a message.");
+    //    pub.publish("a nice channel", "I am sending a second message.");
+    //    pub.publish("a nice channel", "I am sending my last message.");
     //});
-    //sub.subscribe('getBt');
-
-
-    var msg_count = 0;
-
-    sub.on("subscribe", function (channel, count) {
-        pub.publish("a nice channel", "I am sending a message.");
-        pub.publish("a nice channel", "I am sending a second message.");
-        pub.publish("a nice channel", "I am sending my last message.");
-    });
-
-    sub.on("message", function (channel, message) {
-        console.log("sub channel " + channel + ": " + message);
-        msg_count += 1;
-        if (msg_count === 3) {
-            sub.unsubscribe();
-            sub.quit();
-            pub.quit();
-        }
-    });
-
-    sub.subscribe("a nice channel");
+    //
+    //sub.on("message", function (channel, message) {
+    //    console.log("sub channel " + channel + ": " + message);
+    //    msg_count += 1;
+    //    if (msg_count === 3) {
+    //        sub.unsubscribe();
+    //        sub.quit();
+    //        pub.quit();
+    //    }
+    //});
+    //
+    //sub.subscribe("a nice channel");
 
 
 
