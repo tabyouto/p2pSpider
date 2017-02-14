519afd241711ceff8a8717282bf62366692b9c0a
chihaiyue <chihaiyue@111.com.cn>
2017-02-14 17:31:54 +0800

    add

--- test/loo.js
+++ test/loo.js
@@ -5,14 +5,17 @@ var redis = require("redis");
 var client = redis.createClient();
 var i = 0;
 function sql() {
-	client.lrange('p2pData', 0, i, function(err, reply) {
+	client.lrange('p2pData', 0, 0, function(err, reply) {
 		console.log(reply); // ['angularjs', 'backbone']
 		if(reply) {
-			sqlAction.insert('INSERT IGNORE INTO list(name,magnet,infoHash,size,catch_date,hot,download_count,file_number,content_file) VALUES ?',[JSON.parse(reply[0])],function (err, vals, fields) {
-				i++;
-				console.log(i);
-				sql();
+			client.LPOP('p2pData',function(v) {
+
 			});
+			//sqlAction.insert('INSERT IGNORE INTO list(name,magnet,infoHash,size,catch_date,hot,download_count,file_number,content_file) VALUES ?',[JSON.parse(reply[0])],function (err, vals, fields) {
+			//	i++;
+			//	console.log(i);
+			//	sql();
+			//});
 		}
 	});
 }
