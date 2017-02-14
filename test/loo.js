1d7ddb8f612e7fd4445ec85255b82395df5f90b5
chihaiyue <chihaiyue@111.com.cn>
2017-02-14 17:32:45 +0800

    add

--- test/loo.js
+++ test/loo.js
@@ -14,7 +14,7 @@ function sql() {
 			//sqlAction.insert('INSERT IGNORE INTO list(name,magnet,infoHash,size,catch_date,hot,download_count,file_number,content_file) VALUES ?',[JSON.parse(reply[0])],function (err, vals, fields) {
 			//	i++;
 			//	console.log(i);
-			//	sql();
+				sql();
 			//});
 		}
 	});
