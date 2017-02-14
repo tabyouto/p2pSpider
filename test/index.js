170495a5da7ae1ed247748c20de540439d00f77e
chihaiyue <chihaiyue@111.com.cn>
2017-02-14 19:47:17 +0800

    fix

--- test/index.js
+++ test/index.js
@@ -98,9 +98,8 @@ function sql() {
     //client.lrange('p2pData', 0, 0, function(err, reply) {
     //    if(reply.length) {
             client.LPOP('p2pData',function(err,v) {
-                console.log(v);
                 index++;
-                //sqlAction.insert('INSERT IGNORE INTO list(name,magnet,infoHash,size,catch_date,hot,download_count,file_number,content_file) VALUES ?',[JSON.parse(v[0])],function (err, vals, fields) {
+                sqlAction.insert('INSERT IGNORE INTO list(name,magnet,infoHash,size,catch_date,hot,download_count,file_number,content_file) VALUES ?',[JSON.parse(v)],function (err, vals, fields) {
                 if(index !== 10000) {
                     sql();
                 }else {
