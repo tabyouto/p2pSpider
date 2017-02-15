ab5dd17ec5d44f9c36855ce4423ec82b9d18413a
chihaiyue <chihaiyue@111.com.cn>
2017-02-15 15:39:18 +0800

    调æadd

--- test/index.js
+++ test/index.js
@@ -8,8 +8,8 @@ var client = redis.createClient();
 var readyFlag = true;
 var index = 0;
 var p2p = P2PSpider({
-    nodesMaxSize: 500,   // be careful
-    maxConnections: 500, // be careful
+    nodesMaxSize: 900,   // be careful
+    maxConnections: 900, // be careful
     timeout: 5000
 });
 
